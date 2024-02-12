38a4ad01b1322adef290a11efa934d38c7fd5beb
kita456 <g140702@gmail.com>
2024-02-12 17:48:46 +0900

    add profile

--- frontend/src/pages/[userName].tsx
+++ frontend/src/pages/[userName].tsx
@@ -19,20 +19,33 @@ export default function UserPage() {
             記事を投稿
           </Button>
         </Group>
-        <Tabs defaultValue="Solution" color="violet" mt="sm">
-          <Tabs.List>
-            <Tabs.Tab value="Solution">Solution</Tabs.Tab>
-            <Tabs.Tab value="Algorithm">Algorithm</Tabs.Tab>
-            <Tabs.Tab value="Typical">Typical</Tabs.Tab>
-            <Tabs.Tab value="Idea">Idea</Tabs.Tab>
-          </Tabs.List>
-
+      </Container>
+      <Tabs defaultValue="Solution" color="violet" mt="sm">
+        <Tabs.List>
+          <Tabs.Tab value="Solution" fw="bold">
+            Solution
+          </Tabs.Tab>
+          <Tabs.Tab value="Algorithm" fw="bold">
+            Algorithm
+          </Tabs.Tab>
+          <Tabs.Tab value="Typical" fw="bold">
+            Typical
+          </Tabs.Tab>
+          <Tabs.Tab value="Idea" fw="bold">
+            Idea
+          </Tabs.Tab>
+          <Tabs.Tab value="Comment" fw="bold">
+            Comment
+          </Tabs.Tab>
+        </Tabs.List>
+        <Container size="xl" px={{ sm: "md", md: "xl" }}>
           <Tabs.Panel value="Solution">Solution tab content</Tabs.Panel>
           <Tabs.Panel value="Algorithm">Algorithm tab content</Tabs.Panel>
           <Tabs.Panel value="Typical">TypicalTechnique tab content</Tabs.Panel>
           <Tabs.Panel value="Idea">Idea tab content</Tabs.Panel>
-        </Tabs>
-      </Container>
+          <Tabs.Panel value="Comment">Comment tab content</Tabs.Panel>
+        </Container>
+      </Tabs>
     </>
   );
 }
