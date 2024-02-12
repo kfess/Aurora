8a818cefcd42c4b71e79c366d6f2754b1ecbc2d4
kita456 <g140702@gmail.com>
2024-02-12 17:40:56 +0900

    add profile

--- frontend/src/pages/[userName].tsx
+++ frontend/src/pages/[userName].tsx
@@ -1,4 +1,4 @@
-import { Button, Container, Group } from "@mantine/core";
+import { Button, Container, Group, Tabs } from "@mantine/core";
 import { HiOutlinePencilAlt } from "react-icons/hi";
 import { OverlapUserInfo } from "@/features/profile/components/OverlapUserInfo";
 
@@ -19,6 +19,19 @@ export default function UserPage() {
             記事を投稿
           </Button>
         </Group>
+        <Tabs defaultValue="Solution" color="violet" mt="sm">
+          <Tabs.List>
+            <Tabs.Tab value="Solution">Solution</Tabs.Tab>
+            <Tabs.Tab value="Algorithm">Algorithm</Tabs.Tab>
+            <Tabs.Tab value="Typical">Typical</Tabs.Tab>
+            <Tabs.Tab value="Idea">Idea</Tabs.Tab>
+          </Tabs.List>
+
+          <Tabs.Panel value="Solution">Solution tab content</Tabs.Panel>
+          <Tabs.Panel value="Algorithm">Algorithm tab content</Tabs.Panel>
+          <Tabs.Panel value="Typical">TypicalTechnique tab content</Tabs.Panel>
+          <Tabs.Panel value="Idea">Idea tab content</Tabs.Panel>
+        </Tabs>
       </Container>
     </>
   );
