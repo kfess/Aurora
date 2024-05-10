40328346b6d5c0995c9bf9c5fa3f5a5fbf3b46d8
kita456 <g140702@gmail.com>
2024-05-11 00:03:06 +0900

    add

--- api/src/infra/api/yuki/api_client.rs
+++ api/src/infra/api/yuki/api_client.rs
@@ -1,18 +1,17 @@
 use anyhow::{Ok, Result};
+
 use chrono::DateTime;
-use core::time;
 use std::sync::Arc;
-use std::thread;
+use std::time::Duration;
 use std::{collections::HashMap, sync::RwLock};
 
-use crate::domain::{contest::Contest, problem::Problem, value_object::platform::Platform};
+use crate::domain::{contest::Contest, problem::Problem, vo::platform::Platform};
+use crate::utils::api::{get_json, sleep};
 use crate::utils::format::num_to_alphabet;
 
-use self::types::{
+use crate::infra::api::yuki::types::{
     YukicoderContest, YukicoderProblem, YukicoderProblemWithStatistics, YukicoderTag,
 };
-
-use super::*;
 
 const YUKICODER_URL: &'static str = "https://yukicoder.me/api/v1";
 
@@ -31,26 +30,14 @@ impl YukicoderAPIClient {
 
     async fn fetch_problem(&self, problem_id: u64) -> Result<YukicoderProblemWithStatistics> {
         let url = format!("{}/problems/{}", YUKICODER_URL, problem_id);
-        let response = self.client.get(url).send().await?;
-
-        if !response.status().is_success() {
-            return Err(anyhow::anyhow!("Failed to fetch yukicoder problem"));
-        }
-
-        let problem = response.json::<YukicoderProblemWithStatistics>().await?;
+        let problem = get_json(&url, &self.client).await?;
 
         Ok(problem)
     }
 
     async fn fetch_problem_ids(&self) -> Result<Vec<u64>> {
         let url = format!("{}/problems", YUKICODER_URL);
-        let response = self.client.get(url).send().await?;
-
-        if !response.status().is_success() {
-            return Err(anyhow::anyhow!("Failed to fetch yukicoder problems"));
-        }
-
-        let problems = response.json::<Vec<YukicoderProblem>>().await?;
+        let problems: Vec<YukicoderProblem> = get_json(&url, &self.client).await?;
 
         let problem_ids = problems
             .iter()
@@ -62,24 +49,11 @@ impl YukicoderAPIClient {
 
     async fn fetch_past_contests(&self) -> Result<Vec<YukicoderContest>> {
         let url = format!("{}/contest/past", YUKICODER_URL);
-        let response = self.client.get(url).send().await?;
+        let mut contests: Vec<YukicoderContest> = get_json(&url, &self.client).await?;
 
-        if !response.status().is_success() {
-            return Err(anyhow::anyhow!("Failed to fetch yukicoder contests"));
-        }
-
-        let contests = response
-            .json::<Vec<YukicoderContest>>()
-            .await?
-            .iter()
-            .map(|contest| YukicoderContest {
-                id: contest.id,
-                name: contest.name.trim().to_string(),
-                date: contest.date.clone(),
-                end_date: contest.end_date.clone(),
-                problem_id_list: contest.problem_id_list.clone(),
-            })
-            .collect();
+        contests.iter_mut().for_each(|contest| {
+            contest.name = contest.name.trim().to_string();
+        });
 
         Ok(contests)
     }
@@ -87,13 +61,7 @@ impl YukicoderAPIClient {
     #[allow(dead_code)]
     async fn fetch_future_contests(&self) -> Result<Vec<YukicoderContest>> {
         let url = format!("{}/contest/future", YUKICODER_URL);
-        let response = self.client.get(url).send().await?;
-
-        if !response.status().is_success() {
-            return Err(anyhow::anyhow!("Failed to fetch yukicoder contests"));
-        }
-
-        let future_contests = response.json::<Vec<YukicoderContest>>().await?;
+        let future_contests: Vec<YukicoderContest> = get_json(&url, &self.client).await?;
 
         Ok(future_contests)
     }
@@ -101,103 +69,60 @@ impl YukicoderAPIClient {
     #[allow(dead_code)]
     async fn fetch_tags(&self) -> Result<Vec<YukicoderTag>> {
         let url = format!("{}/statistics/tags", YUKICODER_URL);
-        let response = self.client.get(url).send().await?;
-
-        if !response.status().is_success() {
-            return Err(anyhow::anyhow!("Failed to fetch yukicoder tags"));
-        }
-
-        let tags = response.json::<Vec<YukicoderTag>>().await?;
+        let tags = get_json(&url, &self.client).await?;
 
         Ok(tags)
     }
 
-    fn build_problem(
-        &self,
-        contest_name: &str,
-        index: &str,
-        problem: &YukicoderProblemWithStatistics,
-    ) -> Problem {
-        Problem::reconstruct(
-            contest_name.to_string(),
-            index.to_string(),
-            problem.title.to_string(),
-            Platform::Yukicoder,
-            Some(problem.level),
-            Option::None,
-            problem.tags.split(",").map(|s| s.to_string()).collect(),
-            format!("https://yukicoder.me/problems/no/{}", problem.no),
-            Some(problem.statistics.solved),
-            Some(problem.statistics.total),
-        )
-    }
-
-    fn build_contest(&self, contest: &YukicoderContest, problems: Vec<Problem>) -> Contest {
-        let start_timestamp = DateTime::parse_from_rfc3339(&contest.date)
-            .unwrap()
-            .timestamp() as u64;
-
-        let duration_seconds = DateTime::parse_from_rfc3339(&contest.end_date)
-            .unwrap()
-            .timestamp() as u64
-            - start_timestamp;
-
-        Contest::reconstruct(
-            contest.name.to_string(),
-            Platform::Yukicoder,
-            "finished".to_string(),
-            Some(start_timestamp),
-            Some(duration_seconds),
-            format!("https://yukicoder.me/contests/{}", contest.id),
-            problems,
-        )
-    }
-
-    async fn merge(&self) -> Result<()> {
+    async fn build_problems_contests(&self) -> Result<()> {
         if self.cache.read().unwrap().is_some() {
             return Ok(());
         }
 
-        let (fetched_problem_ids, fetched_contests) =
-            tokio::try_join!(self.fetch_problem_ids(), self.fetch_past_contests()).unwrap();
+        let (raw_problem_ids, raw_contests) =
+            tokio::try_join!(self.fetch_problem_ids(), self.fetch_past_contests())?;
 
-        let (mut problems, mut contests): (Vec<Problem>, Vec<Contest>) = (vec![], vec![]);
+        let mut problems: Vec<Problem> = vec![];
         let mut problem_id_map: HashMap<u64, (String, String)> = HashMap::new();
         let mut contest_problems_map: HashMap<String, Vec<Problem>> = HashMap::new();
 
-        for contest in fetched_contests.iter() {
-            let mut problem_ids = contest.problem_id_list.clone();
-            problem_ids.sort_unstable();
-            for (index, problem_id) in problem_ids.iter().enumerate() {
-                let letter = num_to_alphabet(index);
-                problem_id_map.insert(*problem_id, (contest.name.to_string(), letter));
+        for c in raw_contests.iter() {
+            for (idx, problem_id) in c.problem_id_list.iter().enumerate() {
+                problem_id_map.insert(*problem_id, (c.name.to_string(), num_to_alphabet(idx)));
             }
         }
 
-        for problem_id in fetched_problem_ids[..100].iter() {
-            let fetched_problem = self.fetch_problem(*problem_id).await.unwrap();
+        for problem_id in &raw_problem_ids[..100] {
+            let raw_problem = self.fetch_problem(*problem_id).await?;
 
-            let default_value = ("Unknown".to_string(), "A".to_string());
-            let (contest_name, index) = problem_id_map.get(problem_id).unwrap_or(&default_value);
+            let (contest_name, idx) = problem_id_map
+                .get(problem_id)
+                .cloned()
+                .unwrap_or_else(|| ("Unknown".to_string(), "A".to_string()));
 
-            let problem = self.build_problem(contest_name, index, &fetched_problem);
-
+            let problem = build_problem(&contest_name, &idx, &raw_problem);
             problems.push(problem.clone());
 
-            let contest_id = format!("{}_{}", String::from(Platform::Yukicoder), contest_name);
             contest_problems_map
-                .entry(contest_id.clone())
+                .entry(format!(
+                    "{}_{}",
+                    String::from(Platform::Yukicoder),
+                    contest_name
+                ))
                 .or_insert_with(Vec::new)
                 .push(problem.clone());
 
-            thread::sleep(time::Duration::from_millis(1000));
+            sleep(Duration::from_secs(1)).await;
         }
 
-        for contest in fetched_contests.iter() {
-            let contest_id = format!("{}_{}", String::from(Platform::Yukicoder), contest.name);
-            contests.push(self.build_contest(
-                contest,
-                contest_problems_map.get(&contest_id).cloned().unwrap(),
+        let mut contests: Vec<Contest> = vec![];
+        for c in raw_contests.iter() {
+            contests.push(build_contest(
+                c,
+                contest_problems_map
+                    .get(&format!("{}_{}", String::from(Platform::Yukicoder), c.name))
+                    .cloned()
+                    .unwrap(),
             ));
         }
 
@@ -214,7 +139,7 @@ pub trait IYukicoderAPIClient {
 
 impl IYukicoderAPIClient for YukicoderAPIClient {
     async fn get_problems(&self) -> Result<Vec<Problem>> {
-        self.merge().await?;
+        self.build_problems_contests().await?;
         let cache = self.cache.read().unwrap();
         let (problems, _) = cache.as_ref().unwrap();
 
@@ -222,10 +147,50 @@ impl IYukicoderAPIClient for YukicoderAPIClient {
     }
 
     async fn get_contests(&self) -> Result<Vec<Contest>> {
-        self.merge().await?;
+        self.build_problems_contests().await?;
         let cache = self.cache.read().unwrap();
         let (_, contests) = cache.as_ref().unwrap();
 
         Ok(contests.clone())
     }
+}
+
+pub fn build_problem(
+    contest_name: &str,
+    index: &str,
+    problem: &YukicoderProblemWithStatistics,
+) -> Problem {
+    Problem::reconstruct(
+        contest_name.to_string(),
+        index.to_string(),
+        problem.title.to_string(),
+        Platform::Yukicoder,
+        Some(problem.level),
+        Option::None,
+        problem.tags.split(",").map(|s| s.to_string()).collect(),
+        format!("https://yukicoder.me/problems/no/{}", problem.no),
+        Some(problem.statistics.solved),
+        Some(problem.statistics.total),
+    )
+}
+
+pub fn build_contest(contest: &YukicoderContest, problems: Vec<Problem>) -> Contest {
+    let start_timestamp = DateTime::parse_from_rfc3339(&contest.date)
+        .unwrap()
+        .timestamp() as u64;
+
+    let duration_seconds = DateTime::parse_from_rfc3339(&contest.end_date)
+        .unwrap()
+        .timestamp() as u64
+        - start_timestamp;
+
+    Contest::reconstruct(
+        contest.name.to_string(),
+        Platform::Yukicoder,
+        "finished".to_string(),
+        Some(start_timestamp),
+        Some(duration_seconds),
+        format!("https://yukicoder.me/contests/{}", contest.id),
+        problems,
+    )
 }
