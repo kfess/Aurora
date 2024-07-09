2a5c74d99130e79fcb3d439ad8210cabc4f95e4e
kita456 <g140702@gmail.com>
2024-07-09 18:59:37 +0900

    setup frontend

--- frontend/src/components/SocialLink/types.ts
+++
@@ -1,6 +0,0 @@
-export type SocialMediaType = "GitHub" | "X" | "Facebook" | "Discord" | "RSS";
-export type GeneralLinkType = "HomePage";
-
-export type SocialLinkProps =
-  | { type: SocialMediaType; userName: string; size?: number; color?: string }
-  | { type: GeneralLinkType; url: string; size?: number; color?: string };
--- frontend/src/types/types.ts
+++
@@ -1,9 +0,0 @@
-export type Platform =
-  | "Atcoder"
-  | "Codeforces"
-  | "AizuOnlineJudge"
-  | "yukicoder"
-  | "Library Checker";
-
-export type PlatformUserName = `${Uncapitalize<Platform>}UserName`;
-
